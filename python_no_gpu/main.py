65343d4c754f4e9382b49c459bd32628b691355a
mithulcb <mithulcb@gmail.com>
2023-05-03 21:38:36 +0530

    updating_ui

--- python_no_gpu/main.py
+++ python_no_gpu/main.py
@@ -6,7 +6,7 @@ import gpt2
 
 root = tk.Tk()
 
-root.title("Caption")
+root.title("Caption Generator")
 
 root.geometry("1000x500")
 
@@ -69,7 +69,7 @@ def delet():
 welcome= tk.Label(root, text="Welcome.\n\nImage Caption Generator.",font=('Helevetica',24))
 welcome.pack()
 
-browse_button = tk.Button(root, text="Browse For Image", command=upload_image)
+browse_button = tk.Button(root, text="Browse For Image", command=upload_image,fg="white",bg="black",activebackground="white",activeforeground="black")
 browse_button.pack()
 
 fp=tk.Text(root,height=1,width=10)
@@ -81,14 +81,14 @@ n.pack()
 t=tk.Text(root,height=1,width=10)
 t.pack()
 
-button = tk.Button(root, text="Display Image", command=display)
+button = tk.Button(root, text="Display Image", command=display,fg="white",bg="black",activebackground="white",activeforeground="black")
 button.pack()
 
-cap_button = tk.Button(root, text="Captions", command=captions)
+cap_button = tk.Button(root, text="Captions", command=captions,fg="white",bg="black",activebackground="white",activeforeground="black")
 cap_button.pack()
 
 
-del_button = tk.Button(root, text="Clear", command=delet)
+del_button = tk.Button(root, text="Clear", command=delet,fg="white",bg="black",activebackground="white",activeforeground="black")
 del_button.pack()
 
 hi=tk.Text(root,height=10,width=80)
